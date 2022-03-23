3f91076732e28790717b27b8aa95076821f2e9b9
stgoneira <stgoneiran@gmail.com>
2022-03-23 07:50:52 -0300

    ppt list

---
+++ ppt/makeindex.rb
@@ -0,0 +1,11 @@
+archivos    = Dir.entries(".")
+noDeseados  = [".", "..", "makeindex.rb"]
+ppts        = archivos - noDeseados
+baseUrl     = "https://stgoneira.github.io/introduccion-programacion-ruby/ppt/"
+
+File.open("README.md", "w") { |f|
+    f.write "# Presentaciones\n\n"
+    ppts.each_with_index { |ppt, index|
+        f.write "#{index}. [#{ppt}](#{baseUrl}#{ppt}) \n"
+    }
+}
