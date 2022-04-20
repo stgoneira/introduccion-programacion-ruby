b5841d2c8132a25ad5001b7202f94fa7b0d463bd
hcslearning <learning@hardcybersoft.cl>
2022-04-20 13:05:59 -0400

    mindmap

--- codigo/loops.rb
+++ codigo/loops.rb
@@ -1,4 +1,8 @@
 personas = ["Juanito", "Pedrito", "Ramoncito"]
 personas.each_with_index do |persona, indice|
-	puts "#{indice}.- #{persona}"
+	# puts "#{indice}.- #{persona}"
 end
+
+for persona in personas do 
+	puts "#{persona}"
+end 
